bfc0a385b34432ccfd1896857968ce68319faec3
Sualeh Asif <sualeh@mit.edu>
2022-09-05 15:33:22 -0400

    added background that compiles to public

--- public/background.js
+++ public/background.js
@@ -1,1 +1,12 @@
-console.log("hi i am background script");
+"use strict";
+// Function: update
+// Description: check the state of tabs in chrome.
+//              create a new tab if there is no tab with the url.
+function update(url) {
+    chrome.tabs.query({ url: "https://www.google.com/" }, function (tabs) {
+        if (tabs.length == 0) {
+            chrome.tabs.create({ url: "https://www.google.com/" });
+        }
+    });
+}
+//# sourceMappingURL=background.js.map
