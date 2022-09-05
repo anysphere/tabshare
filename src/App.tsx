aef4461b00bb9a385fc964533c76cb82c1eacb0d
Sualeh Asif <sualeh@mit.edu>
2022-09-05 13:36:24 -0400

    basic sync code in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 ///<reference types="chrome"/>
-import './App.css';
-import React from 'react';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actions } from "@liveblocks/redux";
 
-function App() {
+import "./App.css";
+
+function WhoIsHere() {
+  const othersUsersCount = useSelector(
+    (state) => state.liveblocks.others.length
+  );
+
   return (
-    <div>
-      hi
+    <div className="who_is_here">
+      There are {othersUsersCount} other users online
     </div>
   );
 }
 
-export default App
+export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(
+      actions.enterRoom("redux-demo-room", {
+        todos: [],
+      })
+    );
+
+    return () => {
+      dispatch(actions.leaveRoom("redux-demo-room"));
+    };
+  }, [dispatch]);
+
+  return (
+    <div className="container">
+      <h1>Liveblocks Redux Demo</h1>
+      <WhoIsHere />
+    </div>
+  );
+}
